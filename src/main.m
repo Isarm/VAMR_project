d96da93f168d672a728c97f9217b9ee938236089
Isar <isarmeijer@gmail.com>
2021-11-18 16:43:38 +0100

    Change string concatenations to strcat

--- src/main.m
+++ src/main.m
@@ -1,10 +1,14 @@
 %% Setup
-ds = 2; % 0: KITTI, 1: Malaga, 2: parking
+ds = 0; % 0: KITTI, 1: Malaga, 2: parking
+
+kitti_path = "data/kitti05/kitti";
+malaga_path = "data/malaga-urban-dataset-extract-07";
+parking_path = "data/parking";
 
 if ds == 0
     % need to set kitti_path to folder containing "05" and "poses"
     assert(exist('kitti_path', 'var') ~= 0);
-    ground_truth = load([kitti_path '/poses/05.txt']);
+    ground_truth = load(strcat(kitti_path, '/poses/05.txt'));
     ground_truth = ground_truth(:, [end-8 end]);
     last_frame = 4540;
     K = [7.188560000000e+02 0 6.071928000000e+02
@@ -13,8 +17,8 @@ if ds == 0
 elseif ds == 1
     % Path containing the many files of Malaga 7.
     assert(exist('malaga_path', 'var') ~= 0);
-    images = dir([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images']);
+    images = dir(strcat(malaga_path, ...
+        '/malaga-urban-dataset-extract-07_rectified_800x600_Images'));
     left_images = images(3:2:end);
     last_frame = length(left_images);
     K = [621.18428 0 404.0076
@@ -24,33 +28,34 @@ elseif ds == 2
     % Path containing images, depths and all...
     assert(exist('parking_path', 'var') ~= 0);
     last_frame = 598;
-    K = load([parking_path '/K.txt']);
+    K = load(strcat(parking_path, '/K.txt'));
      
-    ground_truth = load([parking_path '/poses.txt']);
+    ground_truth = load(strcat(parking_path, '/poses.txt'));
     ground_truth = ground_truth(:, [end-8 end]);
 else
     assert(false);
 end
 
+
 %% Bootstrap
 % need to set bootstrap_frames
 if ds == 0
-    img0 = imread([kitti_path '/05/image_0/' ...
-        sprintf('%06d.png',bootstrap_frames(1))]);
-    img1 = imread([kitti_path '/05/image_0/' ...
-        sprintf('%06d.png',bootstrap_frames(2))]);
+    img0 = imread(strcat(kitti_path, '/05/image_0/', ...
+        sprintf('%06d.png',bootstrap_frames(1))));
+    img1 = imread(strcat(kitti_path, '/05/image_0/', ...
+        sprintf('%06d.png',bootstrap_frames(2))));
 elseif ds == 1
-    img0 = rgb2gray(imread([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-        left_images(bootstrap_frames(1)).name]));
-    img1 = rgb2gray(imread([malaga_path ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-        left_images(bootstrap_frames(2)).name]));
+    img0 = rgb2gray(imread(strcat(malaga_path, ...
+        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
+        left_images(bootstrap_frames(1)).name)));
+    img1 = rgb2gray(imread(strcat(malaga_path, ...
+        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
+        left_images(bootstrap_frames(2)).name)));
 elseif ds == 2
-    img0 = rgb2gray(imread([parking_path ...
-        sprintf('/images/img_%05d.png',bootstrap_frames(1))]));
-    img1 = rgb2gray(imread([parking_path ...
-        sprintf('/images/img_%05d.png',bootstrap_frames(2))]));
+    img0 = rgb2gray(imread(strcat(parking_path, ...
+        sprintf('/images/img_%05d.png',bootstrap_frames(1)))));
+    img1 = rgb2gray(imread(strcat(parking_path, ...
+        sprintf('/images/img_%05d.png',bootstrap_frames(2)))));
 else
     assert(false);
 end
@@ -60,14 +65,14 @@ range = (bootstrap_frames(2)+1):last_frame;
 for i = range
     fprintf('\n\nProcessing frame %d\n=====================\n', i);
     if ds == 0
-        image = imread([kitti_path '/05/image_0/' sprintf('%06d.png',i)]);
+        image = imread(strcat(kitti_path, '/05/image_0/', sprintf('%06d.png',i)));
     elseif ds == 1
-        image = rgb2gray(imread([malaga_path ...
-            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
-            left_images(i).name]));
+        image = rgb2gray(imread(strcat(malaga_path, ...
+            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
+            left_images(i).name)));
     elseif ds == 2
-        image = im2uint8(rgb2gray(imread([parking_path ...
-            sprintf('/images/img_%05d.png',i)])));
+        image = im2uint8(rgb2gray(imread(strcat(parking_path, ...
+            sprintf('/images/img_%05d.png',i)))));
     else
         assert(false);
     end
