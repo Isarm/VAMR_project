76b8df1419f5d6ca54e1a6d2e2e7d44640630a3a
Joshua O'Reilly <joshua@joshuaoreilly.com>
2021-12-21 14:09:29 -0500

    Add updateFigure.m, add scaling, image support, amongst others
    Big ol commit which adds images with point annotation, two sets of points for the top view plot (since it needs vehicle position and 3D landmark location), titles (mostly, see TODO in createFigure.m), and rebuids testPlotting.m to test all this

--- src/createFigure.m
+++ src/createFigure.m
@@ -1,21 +1,51 @@
-function fig = createFigure(numPointsNumLandmarks)
+function [fig, topViewLandmarksX, topViewLandmarksY, topViewCarX, topViewCarY] = createFigure(numPointsNumLandmarks, numFrames)
+    %CREATEFIGURE
+    % Creates and returns a figure which can be updated with each frame processed
+
+    % Inputs
+    %
+    %   numPointsNumLandmarks   = number of points to show in the Number of Landmarks plot before sliding the window
+    %   numFrames               = number of frame data to show in the top-view plot
+    
+    % Outputs
+    %
+    %   fig                 = figure itself
+    %   topViewLandmarksX   = cell array to update landmarks to show in last numFrames frames
+    %   topViewLandmarksY   = cell array to update landmarks to show in last numFrames frames
+    %   topViewCarX        = cell array to update car positions to show in last numFrames frames
+    %   topViewCarY        = cell array to update car positions to show in last numFrames frames
+
     fig = struct;
     fig.f = figure;
+    fig.numFrames = numFrames;
 
+    % Top left corner: plot current frame and keypoints or whatever else we'd like
     fig.currentFramePlot = subplot(2,2,1);
-    fig.currentFrameData = [];
+    title('Current Image')
+    hold(fig.currentFramePlot, 'on')
 
+    % Top right corner: plot 3D landmarks and car position as seen from above
     fig.topViewPlot = subplot(2,2,2);
-    % should only be tracking last 20 frames, so need some clever way of tracking and updating these arrays (efficiently)
-    fig.TopViewX = [];
-    fig.TopViewY = [];
+    % TODO: get this title to show?
+    title(sprintf('Trajectory of last %d frames and landmarks', numFrames));
     axis equal;
-
+    % each cell contains the values for a given frame
+    % since matlab is pass by value only, we need to update these outside this function
+    topViewLandmarksX = cell(1,numFrames);
+    topViewLandmarksY = cell(1,numFrames);
+    topViewCarX = cell(1,numFrames);
+    topViewCarY = cell(1,numFrames);
+    
+    % Bottom left corner: number of keypoints tracked over the last numFrames frames
     fig.numLandmarksPlot = subplot(2,2,3);
     fig.numLandmarksData = animatedline('MaximumNumPoints',numPointsNumLandmarks);
+    xlim([-numFrames, 0])
+    title(sprintf('Number of landmarks tracked over last %d frames', numFrames));
+    addpoints(fig.numLandmarksData, 0, 0)
     axis equal;
 
+    % Bottom right corner: position of vehicle over time?
     fig.fullTrajectoryPlot = subplot(2,2,4);
     fig.fullTrajectoryData = animatedline;
-    axis equal;
+    title('Full Trajectory')
 end
--- test/testPlotting.m
+++ test/testPlotting.m
@@ -1,4 +1,7 @@
-fig = createFigure(50);
+numFrames = 20;
+
+[fig, topViewLandmarksX, topViewLandmarksY, topViewCarX, topViewCarY] = createFigure(50, numFrames);
+topViewLastCell = 1;
 
 pause('on')
 
@@ -10,24 +13,31 @@ z = cos(x);
 
 pointX = rand(1,numPoints);
 pointY = rand(1,numPoints);
+pointX2 = rand(1,numPoints);
+pointY2 = rand(1,numPoints);
 
 for i = 1:numel(x)
     pause(0.01);
-
-    % update animatedlines
-    addpoints(fig.numLandmarksData, x(i), y(i))
-    addpoints(fig.fullTrajectoryData, x(i), z(i))
-
-    % update point cloud (animatedlines automatically connects points)
-    fig.TopViewX = [fig.TopViewX, pointX(i)];
-    fig.TopViewY = [fig.TopViewY, pointY(i)];
-    plot(fig.topViewPlot, fig.TopViewX(1:i), fig.TopViewY(1:i), 'o');
 
     % new camera frame
     img = imread(strcat("data/kitti05/kitti", '/05/image_0/', ...
         sprintf('%06d.png',i)));
     % some modification of the image here...
-    imshow(img, 'Parent', fig.currentFramePlot);
 
-    drawnow
+    % annoying work we need to do because pass-by-value, this will be in the final script
+    topViewLandmarksX{topViewLastCell} = pointX(i);
+    topViewLandmarksY{topViewLastCell} = pointY(i);
+    topViewCarX{topViewLastCell} = pointX2(i);
+    topViewCarY{topViewLastCell} = pointY2(i);
+    topViewLastCell = mod(topViewLastCell + 1, numFrames);
+    if topViewLastCell == 0
+        topViewLastCell = 1;
+    end
+
+    % just for test
+    landMarkPositionsX = cell2mat(topViewLandmarksX)' * 1226;
+    landMarkPositionsY = cell2mat(topViewLandmarksY)' * 370;
+
+    % lots of transposes and concatenation because x and y are separate structures in this little test script; when extracting keypoints using matlab's built-in functions, these will already be in the right kx2 format
+    updateFigure(fig, img, [landMarkPositionsX,landMarkPositionsY], y(i), [x(i), z(i)], topViewLandmarksX, topViewLandmarksY, topViewCarX, topViewCarY);
 end
