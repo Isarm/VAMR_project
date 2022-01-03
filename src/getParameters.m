1007e2acd87b00e99d35da27d12daae3bc0b2ecf
justshim <58668136+justshim@users.noreply.github.com>
2022-01-03 10:04:24 -0500

    Initial commit

---
+++ src/getParameters.m
@@ -0,0 +1,12 @@
+function parameters = getParameters(ds)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+if ds == 0
+    parameters = 0;
+elseif ds == 1
+    parameters = 0;
+elseif ds == 2
+    parameters = 0;
+end
+end
+
--- src/main.m
+++ src/main.m
@@ -1,64 +1,27 @@
-%% Setup
+%% VAMR Mini-Project
+% ...
+
+%% 
+clc
+clear
+close all
+
+%% [INPUT] 
+% Dataset Selection
 ds = 0; % 0: KITTI, 1: Malaga, 2: parking
 
-kitti_path = "data/kitti05/kitti";
-malaga_path = "data/malaga-urban-dataset-extract-07";
-parking_path = "data/parking";
+% TODO: For each datast, different bootstrap frames
+bootstrapFrame1 = 0;
+bootstrapFrame2 = 2;
 
-if ds == 0
-    % need to set kitti_path to folder containing "05" and "poses"
-    assert(exist('kitti_path', 'var') ~= 0);
-    ground_truth = load(strcat(kitti_path, '/poses/05.txt'));
-    ground_truth = ground_truth(:, [end-8 end]);
-    last_frame = 4540;
-    K = [7.188560000000e+02 0 6.071928000000e+02
-        0 7.188560000000e+02 1.852157000000e+02
-        0 0 1];
-elseif ds == 1
-    % Path containing the many files of Malaga 7.
-    assert(exist('malaga_path', 'var') ~= 0);
-    images = dir(strcat(malaga_path, ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images'));
-    left_images = images(3:2:end);
-    last_frame = length(left_images);
-    K = [621.18428 0 404.0076
-        0 621.18428 309.05989
-        0 0 1];
-elseif ds == 2
-    % Path containing images, depths and all...
-    assert(exist('parking_path', 'var') ~= 0);
-    last_frame = 598;
-    K = load(strcat(parking_path, '/K.txt'));
-     
-    ground_truth = load(strcat(parking_path, '/poses.txt'));
-    ground_truth = ground_truth(:, [end-8 end]);
-else
-    assert(false);
-end
+%% Parameters
+parameters = getParameters(ds);
 
+%% Initialize Figures
+[fig, topViewLandmarksX, topViewLandmarksZ, topViewCarX, topViewCarZ] = createFigure(50, numFrames);
 
 %% Bootstrap
-% need to set bootstrap_frames
-if ds == 0
-    img0 = imread(strcat(kitti_path, '/05/image_0/', ...
-        sprintf('%06d.png',bootstrap_frames(1))));
-    img1 = imread(strcat(kitti_path, '/05/image_0/', ...
-        sprintf('%06d.png',bootstrap_frames(2))));
-elseif ds == 1
-    img0 = rgb2gray(imread(strcat(malaga_path, ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
-        left_images(bootstrap_frames(1)).name)));
-    img1 = rgb2gray(imread(strcat(malaga_path, ...
-        '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
-        left_images(bootstrap_frames(2)).name)));
-elseif ds == 2
-    img0 = rgb2gray(imread(strcat(parking_path, ...
-        sprintf('/images/img_%05d.png',bootstrap_frames(1)))));
-    img1 = rgb2gray(imread(strcat(parking_path, ...
-        sprintf('/images/img_%05d.png',bootstrap_frames(2)))));
-else
-    assert(false);
-end
+[img1, img2, intrinsics] = getInitialFrames(ds, bootstrapFrame1, bootstrapFrame2);
 
 %% Continuous operation
 range = (bootstrap_frames(2)+1):last_frame;
