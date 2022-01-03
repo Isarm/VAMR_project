ec5ed5020ce1f1370e7398710a17fa72af469990
justshim <58668136+justshim@users.noreply.github.com>
2022-01-03 10:33:48 -0500

    Second commit

--- src/getParameters.m
+++ src/getParameters.m
@@ -1,6 +1,25 @@
 function parameters = getParameters(ds)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+
+% Parameters to Set
+
+% KLT 'MaxBidirectionalError'
+% KLT 'MaxIterations'
+% KLT 'BlockSize'
+
+% estimateWorldCameraPose 'MaxNumTrials'
+% estimateWorldCameraPose 'MaxReprojectionError'
+
+% getHarrisFeatures 'MinQuality'
+
+% getSIFTFeatures maxPoints
+% getSIFTFeatures ...
+
+% ismembertol ...
+
+% alpha: Min angle in radians before attempting to triangulate new points
+
 if ds == 0
     parameters = 0;
 elseif ds == 1
--- src/main.m
+++ src/main.m
@@ -6,7 +6,7 @@ clc
 clear
 close all
 
-%% [INPUT] 
+%% [INPUTS] 
 % Dataset Selection
 ds = 0; % 0: KITTI, 1: Malaga, 2: parking
 
@@ -14,33 +14,48 @@ ds = 0; % 0: KITTI, 1: Malaga, 2: parking
 bootstrapFrame1 = 0;
 bootstrapFrame2 = 2;
 
-%% Parameters
-parameters = getParameters(ds);
+numPointsNumLandmarks = 50;
+numFrames = 20;
 
-%% Initialize Figures
-[fig, topViewLandmarksX, topViewLandmarksZ, topViewCarX, topViewCarZ] = createFigure(50, numFrames);
+%% Setup
+parameters = getParameters(ds);
+[img1, img2, intrinsics] = getInitialFrames(ds, bootstrapFrame1, bootstrapFrame2);
+[fig, topViewLandmarksX, topViewLandmarksZ, topViewCarX, topViewCarZ] = createFigure(numPointsNumLandmarks, numFrames);
 
 %% Bootstrap
-[img1, img2, intrinsics] = getInitialFrames(ds, bootstrapFrame1, bootstrapFrame2);
+[keyPoints, landmarks3D, R, T] = bootstrap(img1, img2, intrinsics);
 
 %% Continuous operation
-range = (bootstrap_frames(2)+1):last_frame;
-for i = range
-    fprintf('\n\nProcessing frame %d\n=====================\n', i);
-    if ds == 0
-        image = imread(strcat(kitti_path, '/05/image_0/', sprintf('%06d.png',i)));
-    elseif ds == 1
-        image = rgb2gray(imread(strcat(malaga_path, ...
-            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
-            left_images(i).name)));
-    elseif ds == 2
-        image = im2uint8(rgb2gray(imread(strcat(parking_path, ...
-            sprintf('/images/img_%05d.png',i)))));
-    else
-        assert(false);
-    end
+% Initialize State Variable
+S = struct;
+S.P = keyPoints;
+S.X = landmarks3D;
+S.C = [];
+S.F = [];
+S.T = [];
+
+while true
     % Makes sure that plots refresh.    
     pause(0.01);
-    
-    prev_img = image;
 end
+
+% range = (bootstrap_frames(2)+1):last_frame;
+% for i = range
+%     fprintf('\n\nProcessing frame %d\n=====================\n', i);
+%     if ds == 0
+%         image = imread(strcat(kitti_path, '/05/image_0/', sprintf('%06d.png',i)));
+%     elseif ds == 1
+%         image = rgb2gray(imread(strcat(malaga_path, ...
+%             '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
+%             left_images(i).name)));
+%     elseif ds == 2
+%         image = im2uint8(rgb2gray(imread(strcat(parking_path, ...
+%             sprintf('/images/img_%05d.png',i)))));
+%     else
+%         assert(false);
+%     end
+%     % Makes sure that plots refresh.    
+%     pause(0.01);
+%     
+%     prev_img = image;
+% end
