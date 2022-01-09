b7fb7fd7442283c161066ffeda3992b930d77c47
Isar <isarmeijer@gmail.com>
2022-01-09 17:50:14 +0100

    Copy to main

--- src/main.m
+++ src/main.m
@@ -1,61 +1,112 @@
-%% VAMR Mini-Project
-% ...
-
-%% 
 clc
 clear
 close all
 
-%% [INPUTS] 
-% Dataset Selection
-ds = 4; % 0: KITTI, 1: Malaga, 2: parking
+%% Tests processFrame
+% Can be used to step through the processFrame function
+ds = 0;
 
-% TODO: For each datast, different bootstrap frames
-bootstrapFrame1 = 0;
-bootstrapFrame2 = 2;
+parameters = getParameters(ds);
 
-numPointsNumLandmarks = 50;
+% Get Ground Truth Poses for Parking Datatset
+if ds == 2
+    fid = fopen('data/parking/poses.txt') ;  % open the text file
+    rawPoses = textscan(fid, '%f');   % text scan the data
+    fclose(fid);      % close the file
+    groundTruth = reshape(rawPoses{1}, 12, [])';
+    groundTruthPose = groundTruth(:,[4,8,12]);
+else
+    groundTruthPose = [];
+end
+
 numFrames = 20;
+% x and z since matlab uses x and y for the horizontal plane, and y for up
+% and down
+[fig, topViewLandmarksX, topViewLandmarksZ, topViewCarX, topViewCarZ] = createFigure(50, numFrames, ds);
+topViewLastCell = 1;
 
-%% Setup
-parameters = getParameters();
-[img1, img2, intrinsics] = getInitialFrames(ds, bootstrapFrame1, bootstrapFrame2);
-[fig, topViewLandmarksX, topViewLandmarksZ, topViewCarX, topViewCarZ] = createFigure(numPointsNumLandmarks, numFrames);
+% get first images of dataset, as well as camera instrinsics
+[img1, img2, intrinsics] = getInitialFrames(ds, parameters.bootstrapFrame1, parameters.bootstrapFrame2);
 
-%% Bootstrap
-[keyPoints, landmarks3D, R, T] = bootstrap(img1, img2, intrinsics);
+[keyPoints, landmarks3D, R, T] = bootstrap(img1, img2, intrinsics, parameters);
 
-%% Continuous operation
-% Initialize State Variable
+% Bundle Adjustment
+ba = struct;
+ba.vSet = imageviewset;
+ba.window = 20; % Set to 0 to turn off bundle adjustment
+ba.cameraPoses.ViewId = [];
+ba.cameraPoses.Orientation = {};
+ba.cameraPoses.Location = {};
+ba.xyzPoints = {}; % Organized by frame
+ba.xyzPointsRefined = {};
+ba.sizexyzPoints = [];
+
+ba.refinedPoses = []; 
+ba.reprojectionErrors = [];
+
+if ba.window
+    % Note: FREAK used because extractFeatures cornerPoint objects
+    % default uses FREAK method
+    [F,~] = extractFeatures(img2, keyPoints, 'Method', 'FREAK');
+    ba.vSet = addView(ba.vSet,uint32(parameters.bootstrapFrame2),'Features',F,'Points', keyPoints);
+    ba.xyzPoints = [ba.xyzPoints; landmarks3D];
+    ba.xyzPointsRefined = [ba.xyzPointsRefined; landmarks3D];
+    ba.sizexyzPoints = [ba.sizexyzPoints; size(landmarks3D,1)];
+    % TODO: When parameters.bootstrapFrame2 is not equal to 1
+    ba.cameraPoses.ViewId = [ba.cameraPoses.ViewId; uint32(parameters.bootstrapFrame2)];
+    ba.cameraPoses.Orientation = [ba.cameraPoses.Orientation; R]; % Relative Camera Pose
+    ba.cameraPoses.Location = [ba.cameraPoses.Location; T]; % Relative Camera Pose
+    ba.cameraPoseWC_i.Orientation = {R};
+    ba.cameraPoseWC_i.Location = {T};
+
+end
+
 S = struct;
+
 S.P = keyPoints;
 S.X = landmarks3D;
 S.C = [];
 S.F = [];
 S.T = [];
 
-while true
-    % Makes sure that plots refresh.    
-    pause(0.01);
-end
+i = parameters.bootstrapFrame2 + 1;
 
-% range = (bootstrap_frames(2)+1):last_frame;
-% for i = range
-%     fprintf('\n\nProcessing frame %d\n=====================\n', i);
-%     if ds == 0
-%         image = imread(strcat(kitti_path, '/05/image_0/', sprintf('%06d.png',i)));
-%     elseif ds == 1
-%         image = rgb2gray(imread(strcat(malaga_path, ...
-%             '/malaga-urban-dataset-extract-07_rectified_800x600_Images/', ...
-%             left_images(i).name)));
-%     elseif ds == 2
-%         image = im2uint8(rgb2gray(imread(strcat(parking_path, ...
-%             sprintf('/images/img_%05d.png',i)))));
-%     else
-%         assert(false);
-%     end
-%     % Makes sure that plots refresh.    
-%     pause(0.01);
-%     
-%     prev_img = image;
-% end
+while true
+    img1 = img2;
+    img2 = getFrame(ds, i);
+%     imshow(img2);
+    % I need to track both S1 and S2 temporarily to find
+    % the new 3D landmark points only
+    [S2, T, ba] = processFrame(img1, img2, S, intrinsics, parameters, ba, i);
+    T;
+    i = i + 1;
+    
+    P1 = S.P;
+    P2 = S2.P;
+    [row1,~] = size(P1);
+    [row2,~] = size(P2);
+    numNewKeypoints = row2 - row1;
+
+    X1 = S.X;
+    X2 = S2.X;
+    [row1,~] = size(X1);
+    [row2,~] = size(X2);
+    numNewLandmarks = row2 - row1;
+
+    % TODO: FIX THIS CUS THIS DOES NOT MAKE SENSE
+    topViewLandmarksX{topViewLastCell} = single(X2(:,1)');
+    topViewLandmarksZ{topViewLastCell} = single(X2(:,3)');
+    topViewCarX(i) = T(1,4); % x
+    topViewCarZ(i) = T(3,4); % z
+    topViewLastCell = mod(topViewLastCell + 1, numFrames+1);
+    if topViewLastCell == 0
+        topViewLastCell = 1;
+    end
+
+    updateFigure(fig, img2, i, P2, S2.C, row2,...
+                [T(1,4),T(3,4)], topViewLandmarksX, topViewLandmarksZ, ...
+                topViewCarX, topViewCarZ, ba, groundTruthPose);
+    S = S2;
+    
+    pause(0.1)
+end
